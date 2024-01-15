1f3de87d25a1953fd07780315ac81bc8f4e71709
TranDucTuan <tranductuan291018@gmail.com>
2024-01-15 20:47:45 +0700

    update

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -7,6 +7,102 @@ class UserController {
     let user: IUser[] = await User.find();
     res.json(user);
   }
+  async addUser(req: Request, res: Response) {
+    try {
+      const { firstName, lastName, gender, dob, email, password, phoneNumber, address } = req.body;
+
+      const existingUser = await User.findOne({ email, password });
+
+      if (existingUser) {
+        return res.status(409).json({ message: "User already exists !" });
+      }
+
+      const currentDate = new Date();
+      const newUser = new User({
+        firstName,
+        lastName,
+        gender,
+        dob,
+        email,
+        password,
+        phoneNumber,
+        address,
+        create_at: currentDate,
+        update_at: currentDate,
+      });
+
+      await newUser.save();
+
+      res.status(201).json({ message: "Create Success !" });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+
+  async getAllUsers(req: Request, res: Response) {
+    try {
+      const user = await User.find();
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error !" });
+    }
+  }
+  async getUserById(req: Request, res: Response) {
+    try {
+      const id = req.params.id;
+
+      console.log(id);
+      const user = await User.findOne({ _id: id });
+
+      if (!user) {
+        return res.status(404).json({ message: "User does not exist !" });
+      }
+
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+  async deleteUser(req: Request, res: Response) {
+    try {
+      const id = req.params.id;
+      const user = await User.findOneAndDelete({ _id: id });
+
+      if (!user) {
+        return res.status(404).json({ message: "User does not exist !" });
+      }
+
+      res.status(200).json("Delete Success !");
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error !" });
+    }
+  }
+  async updateUser(req: Request, res: Response) {
+    try {
+      const id = req.params.id;
+      const { firstName, lastName, gender, dob, email, password, phoneNumber, address } = req.body;
+      const currentDate = new Date();
+      const updateData = {
+        firstName,
+        lastName,
+        gender,
+        dob,
+        email,
+        password,
+        phoneNumber,
+        address,
+        update_at: currentDate,
+      };
+      const updatedUser = await User.findOneAndUpdate({ _id: id }, updateData, { new: true });
+      if (!updatedUser) {
+        return res.status(404).json({ message: "User does not exist !" });
+      }
+
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error !" });
+    }
+  }
 }
 
 export default new UserController();
--- server/src/router/userRouter.ts
+++ server/src/router/userRouter.ts
@@ -4,10 +4,14 @@ import userController from "../controllers/userController";
 
 const userRouter = Router();
 
-userRouter.get("/", userController.index);
-// userRouter.get("/", getAllUsers);
+// userRouter.get("/", userController.index);
+userRouter.get("/:id", userController.getUserById);
+userRouter.post("/", userController.addUser);
 // userRouter.get("/:id", getUserById);
-// userRouter.put("/:id", updateUser);
-// userRouter.delete("/:id", deleteUser);
+userRouter.put("/:id",userController.updateUser);
+userRouter.delete("/:id", userController.deleteUser);
+userRouter.get("/", userController.getAllUsers);
+
+
 
 export default userRouter;
