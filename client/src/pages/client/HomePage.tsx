e932386429d3ab9b61507c50d654bcbeaed9447f
TranDucTuan <tranductuan291018@gmail.com>
2024-01-17 18:19:07 +0700

    up

--- client/src/pages/client/HomePage.tsx
+++ client/src/pages/client/HomePage.tsx
@@ -1,11 +1,18 @@
 // import SearchForm from "../../components/client/SearchForm";
+import Footer from "../../components/common/Footer";
 import { Header } from "../../components/index";
+import ProductCarousel from "../../components/client/ProductCarousel "
+import  LoginPage  from "./LoginPage";
 
 const HomePage = (): JSX.Element => {
   return (
     <div className="homepage position-relative">
       <Header />
       {/* <SearchForm /> */}
+      <LoginPage/>
+
+      <ProductCarousel/>
+      <Footer/>
     </div>
   );
 };
--- client/src/pages/client/LoginPage.tsx
+++ client/src/pages/client/LoginPage.tsx
@@ -1,5 +1,28 @@
+import '../../components/client/styles/LoginScreen.scss'
 const LoginPage = (): JSX.Element => {
-  return <div className="loginpage">login page</div>;
+  return ( 
+  <div className="login-screen">
+  <div className="left-section">
+    <h2>Đăng nhập</h2>
+  </div>
+  <div className="right-section">
+    <form>
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" name="email" />
+
+      <label htmlFor="password">Mật khẩu:</label>
+      <input type="password" id="password" name="password" />
+      <div className='row'>
+        <button type="submit">Đăng nhập</button>
+       <div className="additional-options">
+       <span>Quên mật khẩu?</span>
+        <p>Hoặc <span>Đăng ký</span></p>
+       </div>
+        </div>
+      
+    </form>
+  </div>
+</div>);
 };
 
 export default LoginPage;
