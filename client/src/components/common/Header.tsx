a3a0e99085e1ac3eba427c17783c077bb0c3ee71
ChauDz <buichau40@gmail.com>
2024-01-17 18:13:57 +0700

    update

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import NavLink from "../client/NavLink";
 import { FaUserCircle } from "react-icons/fa";
