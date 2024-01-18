2de1e9aabe95d0466c98301a1eb7ddada0ff6ee7
ChauDz <buichau40@gmail.com>
2024-01-18 19:01:55 +0700

    update

--- client/src/pages/client/HomePage.tsx
+++ client/src/pages/client/HomePage.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import ProductCarousel from "../../components/client/ProductCarousel ";
 import "./HomePage.scss";
+import productApi from "../../apis/productApi";
 
 const HomePage = () => {
+  useEffect(() => {
+    const g = async () => {
+      const res = await productApi.getProductById("65a56604d2e19dd6a7095141");
+      console.log(res);
+    };
+    g();
+  }, []);
   return (
     <div className="homepage position-relative">
       <ProductCarousel />
