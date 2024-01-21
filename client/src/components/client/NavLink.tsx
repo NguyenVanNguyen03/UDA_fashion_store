808ed6e1051c900f6f4723c4b88b3e040984a1e5
ChauDz <buichau40@gmail.com>
2024-01-21 18:18:39 +0700

    fix error of nguyen

--- client/src/components/client/NavLink.tsx
+++ client/src/components/client/NavLink.tsx
@@ -1,32 +1,26 @@
 import { Link } from "react-router-dom";
 import { RiArrowDropDownLine } from "react-icons/ri";
-import "./styles/NavLink.scss";
 import { Category } from "../common/Header";
-import { useState } from "react";
+import { screenUrl } from "../../constants/screenUrls";
+import "./styles/NavLink.scss";
 
 const NavLink = (props: Category): JSX.Element => {
   const { name, listDropDown, urlCollections }: Category = props;
-  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
 
-  const handleNameClick = () => {
-    setIsDropDownOpen(!isDropDownOpen);
-  };
   return (
     <li className="navlink position-relative">
       <div className="d-flex align-items-center">
-        <Link to={`/collections/${urlCollections}`} onClick={handleNameClick}>
-          {name}
-        </Link>
-        <RiArrowDropDownLine
-          onClick={handleNameClick}
-          className="btn-dropDown d-flex"
-          fontSize={30}
-        />
+        <Link to={`/collections/${urlCollections}`}>{name}</Link>
+        {listDropDown.length !== 0 ? (
+          <RiArrowDropDownLine className="btn-dropDown d-flex" fontSize={30} />
+        ) : (
+          <div></div>
+        )}
       </div>
       <ul className="dropDown__wrapper position-absolute">
         {listDropDown.map((category, index) => {
           return (
-            <Link key={index} to="#">
+            <Link key={index} to={`${screenUrl.COLLECTIONS}/${category.name}`}>
               {category.name}
             </Link>
           );
